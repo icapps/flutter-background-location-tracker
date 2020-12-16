72900b7c40c9b4c1a5448511ff84a7f8de64b18e
Koen Van Looveren <koen.vanlooveren@icapps.com>
2020-12-16 10:02:34 +0100

    Added 1 test for testing

---
+++ test/src/model/background_location_update_data_test.dart
@@ -0,0 +1,10 @@
+import 'package:background_location_tracker/background_location_tracker.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('Background location updatedata', () async {
+    const data = BackgroundLocationUpdateData(lat: 51.45, lon: 4.5);
+    expect(data.lat, 51.45);
+    expect(data.lon, 4.5);
+  });
+}
