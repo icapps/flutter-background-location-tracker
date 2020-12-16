5c976089cce9ed94408e11451b0613814e579383
Dimmy Maenhout <dimmy.maenhout@icapps.com>
2020-12-16 12:02:27 +0100

    Added background capability (location) + registered BackgroundLocationTrackerPlugin + basic class MethodCallHelper

---
+++ example/ios/Runner/Bridge/MethodCallHelper.swift
@@ -0,0 +1,14 @@
+//
+//  MethodCallHelper.swift
+//  Runner
+//
+//  Created by Dimmy Maenhout on 16/12/2020.
+//
+
+import Foundation
+
+class MethodCallHelper: NSObject, FlutterPlugin {
+    static func register(with registrar: FlutterPluginRegistrar) {
+        
+    }
+}
