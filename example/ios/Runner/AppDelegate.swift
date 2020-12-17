d778697a45a5daf25f023ca5c54761de930c2258
Dimmy Maenhout <dimmy.maenhout@icapps.com>
2020-12-17 13:52:43 +0100

    Renoved plugin

--- example/ios/Runner/AppDelegate.swift
+++ example/ios/Runner/AppDelegate.swift
@@ -5,10 +5,6 @@ import Flutter
 @objc class AppDelegate: FlutterAppDelegate {
     override func application(_ application: UIApplication,didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         GeneratedPluginRegistrant.register(with: self)
-        
-        BackgroundLocationTrackingPlugin.register(with: registrar(forPlugin: "BackgroundLocationTrackingPlugin"))
-        
-        
         return super.application(application, didFinishLaunchingWithOptions: launchOptions)
     }
 }
