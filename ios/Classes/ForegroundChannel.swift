20819796b6b2294f0d98dbd85068c5c70a6c9a46
Koen Van Looveren <koen.vanlooveren@icapps.com>
2020-12-21 10:11:11 +0100

    Fixed some compile issue

--- ios/Classes/ForegroundChannel.swift
+++ ios/Classes/ForegroundChannel.swift
@@ -49,10 +49,12 @@ public class ForegroundChannel : NSObject {
     
     private func initialize(call: FlutterMethodCall, result: @escaping FlutterResult ) {
         let callBackHandleKey = "callback_handle"
-        let channelNameKey = "iOS_config_channel_name"
-        let keys = [callBackHandleKey, channelNameKey]
-        let callbackDispatcherHandleKey = call.arguments
-        SharedPrefsUtil.saveCallBackDispatcherHandleKey(callBackHandle: callbackDispatcherHandleKey as? Int64)
+        let map = call.arguments as? [String: Any]
+        guard let callbackDispatcherHandle = map?[callBackHandleKey] else {
+            result(false)
+            return
+        }
+        SharedPrefsUtil.saveCallBackDispatcherHandleKey(callBackHandle: callbackDispatcherHandle as? Int64)
         result(true)
     }
     
