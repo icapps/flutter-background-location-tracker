0c536db0f02464a39437f302d55620b260c521f1
virtualmarc <virtualmarc@users.noreply.github.com>
2022-12-31 16:32:22 +0100

    #21 Fixed tests

--- test/src/model/background_location_update_data_test.dart
+++ test/src/model/background_location_update_data_test.dart
@@ -3,14 +3,28 @@ import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   test('Background location update data', () async {
-    const data = BackgroundLocationUpdateData(lat: 51.45, lon: 4.5);
+    const data = BackgroundLocationUpdateData(lat: 51.45, lon: 4.5, horizontalAccuracy: 1.2, alt: 42.3, verticalAccuracy: 0.3, course: 128.3, courseAccuracy: 14.3, speed: 12.2, speedAccuracy: 0.9);
     expect(data.lat, 51.45);
     expect(data.lon, 4.5);
+    expect(data.horizontalAccuracy, 1.2);
+    expect(data.alt, 42.3);
+    expect(data.verticalAccuracy, 0.3);
+    expect(data.course, 128.3);
+    expect(data.courseAccuracy, 14.3);
+    expect(data.speed, 12.2);
+    expect(data.speedAccuracy, 0.9);
   });
 
   test('Background location update data', () async {
-    const data = BackgroundLocationUpdateData(lat: 51.45, lon: 4.5);
+    const data = BackgroundLocationUpdateData(lat: 51.45, lon: 4.5, horizontalAccuracy: 1.2, alt: 42.3, verticalAccuracy: 0.3, course: 128.3, courseAccuracy: 14.3, speed: 12.2, speedAccuracy: 0.9);
     expect(data.lat, 51.45);
     expect(data.lon, 4.5);
+    expect(data.horizontalAccuracy, 1.2);
+    expect(data.alt, 42.3);
+    expect(data.verticalAccuracy, 0.3);
+    expect(data.course, 128.3);
+    expect(data.courseAccuracy, 14.3);
+    expect(data.speed, 12.2);
+    expect(data.speedAccuracy, 0.9);
   });
 }
