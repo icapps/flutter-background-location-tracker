3e165b7b6165f8dff62ac71f3d920879583fd155
Rekan Anwar <rekan545@gmail.com>
2024-08-13 18:12:53 +0300

    update BackgroundLocationTrackerPlugin

--- android/src/main/kotlin/com/icapps/background_location_tracker/BackgroundLocationTrackerPlugin.kt
+++ android/src/main/kotlin/com/icapps/background_location_tracker/BackgroundLocationTrackerPlugin.kt
@@ -19,17 +19,20 @@ import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
-import io.flutter.plugin.common.PluginRegistry
 
-class BackgroundLocationTrackerPlugin : FlutterPlugin, MethodCallHandler, ActivityAware {
-    private var lifecycle: Lifecycle? = null
+class BackgroundLocationTrackerPlugin : FlutterPlugin, MethodCallHandler, ActivityAware, Application.ActivityLifecycleCallbacks, LifecycleOwner {
+    private lateinit var lifecycle: LifecycleRegistry
     private var methodCallHelper: MethodCallHelper? = null
+    private var activity: Activity? = null
 
     override fun onAttachedToEngine(binding: FlutterPlugin.FlutterPluginBinding) {
         registerBackgroundLocationManager(binding.binaryMessenger, binding.applicationContext)
     }
 
     override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
+        methodCallHelper?.let {
+            lifecycle.removeObserver(it)
+        }
     }
 
     override fun onMethodCall(call: MethodCall, result: Result) {
@@ -37,28 +40,84 @@ class BackgroundLocationTrackerPlugin : FlutterPlugin, MethodCallHandler, Activi
     }
 
     override fun onAttachedToActivity(binding: ActivityPluginBinding) {
-        lifecycle = FlutterLifecycleAdapter.getActivityLifecycle(binding)
+        activity = binding.activity
+        lifecycle = LifecycleRegistry(this)
+        activity?.application?.registerActivityLifecycleCallbacks(this)
+
         if (methodCallHelper == null) {
             ActivityCounter.attach(binding.activity)
             methodCallHelper = MethodCallHelper.getInstance(binding.activity.applicationContext)
         }
+
         methodCallHelper?.let {
-            lifecycle?.removeObserver(it)
-            lifecycle?.addObserver(it)
+            lifecycle.removeObserver(it)
+            lifecycle.addObserver(it)
         }
     }
 
-    override fun onDetachedFromActivity() {}
+    override fun onDetachedFromActivity() {
+        activity?.application?.unregisterActivityLifecycleCallbacks(this)
+        activity = null
+    }
 
-    override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {}
+    override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
+        onAttachedToActivity(binding)
+    }
 
-    override fun onDetachedFromActivityForConfigChanges() {}
+    override fun onDetachedFromActivityForConfigChanges() {
+        onDetachedFromActivity()
+    }
+
+    override fun getLifecycle(): Lifecycle = lifecycle
+
+    override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {
+        if (activity.hashCode() != this.activity?.hashCode()) {
+            return
+        }
+        lifecycle.handleLifecycleEvent(Lifecycle.Event.ON_CREATE)
+    }
+
+    override fun onActivityStarted(activity: Activity) {
+        if (activity.hashCode() != this.activity?.hashCode()) {
+            return
+        }
+        lifecycle.handleLifecycleEvent(Lifecycle.Event.ON_START)
+    }
+
+    override fun onActivityResumed(activity: Activity) {
+        if (activity.hashCode() != this.activity?.hashCode()) {
+            return
+        }
+        lifecycle.handleLifecycleEvent(Lifecycle.Event.ON_RESUME)
+    }
+
+    override fun onActivityPaused(activity: Activity) {
+        if (activity.hashCode() != this.activity?.hashCode()) {
+            return
+        }
+        lifecycle.handleLifecycleEvent(Lifecycle.Event.ON_PAUSE)
+    }
+
+    override fun onActivityStopped(activity: Activity) {
+        if (activity.hashCode() != this.activity?.hashCode()) {
+            return
+        }
+        lifecycle.handleLifecycleEvent(Lifecycle.Event.ON_STOP)
+    }
+
+    override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) {}
+
+    override fun onActivityDestroyed(activity: Activity) {
+        if (activity.hashCode() != this.activity?.hashCode()) {
+            return
+        }
+        activity.application.unregisterActivityLifecycleCallbacks(this)
+        lifecycle.handleLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+    }
 
     companion object {
         private const val TAG = "FBLTPlugin"
         private const val FOREGROUND_CHANNEL_NAME = "com.icapps.background_location_tracker/foreground_channel"
-
-        var pluginRegistryCallback: PluginRegistry.PluginRegistrantCallback? = null
 
         @JvmStatic
         private fun registerBackgroundLocationManager(messenger: BinaryMessenger, ctx: Context) {
@@ -68,104 +127,9 @@ class BackgroundLocationTrackerPlugin : FlutterPlugin, MethodCallHandler, Activi
                     methodCallHelper = MethodCallHelper.getInstance(ctx)
                 }
                 methodCallHelper?.let {
-                    lifecycle?.removeObserver(it)
-                    lifecycle?.addObserver(it)
-                }
-            })
-        }
-
-        @JvmStatic
-        fun registerWith(registrar: PluginRegistry.Registrar) {
-            val activity = registrar.activity()
-            if (activity == null) {
-                Logger.debug(TAG, "Activity should not be null while registering this plugin")
-                return
-            }
-
-            val lifecycle: Lifecycle = if (activity is LifecycleOwner) {
-                (activity as LifecycleOwner).lifecycle
-            } else {
-                Logger.debug(TAG, "Your activity has not implemented a lifecycle owner. We will create one for you.")
-                @Suppress("DEPRECATION")
-                ProxyLifecycleProvider(activity).lifecycle
-            }
-
-            ActivityCounter.attach(activity)
-            val channel = MethodChannel(registrar.messenger(), FOREGROUND_CHANNEL_NAME)
-            channel.setMethodCallHandler(BackgroundLocationTrackerPlugin().apply {
-                if (methodCallHelper == null) {
-                    methodCallHelper = MethodCallHelper.getInstance(registrar.activeContext())
-                }
-                methodCallHelper?.let {
-                    lifecycle.removeObserver(it)
                     lifecycle.addObserver(it)
                 }
             })
         }
-
-        @Deprecated(message = "Use the Android v2 embedding method.", replaceWith =
-            ReplaceWith("BackgroundLocationTrackerPlugin.pluginRegistryCallback = pluginRegistryCallback")
-        )
-        @JvmStatic
-        fun setPluginRegistrantCallback(pluginRegistryCallback: PluginRegistry.PluginRegistrantCallback) {
-            BackgroundLocationTrackerPlugin.pluginRegistryCallback = pluginRegistryCallback
-        }
-    }
-
-    @Deprecated(message = "Use the Android v2 embedding method.")
-    class ProxyLifecycleProvider internal constructor(activity: Activity) : Application.ActivityLifecycleCallbacks, LifecycleOwner {
-        val lifecycle = LifecycleRegistry(this)
-        private val registrarActivityHashCode: Int = activity.hashCode()
-
-        init {
-            activity.application.registerActivityLifecycleCallbacks(this)
-        }
-
-        override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {
-            if (activity.hashCode() != registrarActivityHashCode) {
-                return
-            }
-            lifecycle.handleLifecycleEvent(Lifecycle.Event.ON_CREATE)
-        }
-
-        override fun onActivityStarted(activity: Activity) {
-            if (activity.hashCode() != registrarActivityHashCode) {
-                return
-            }
-            lifecycle.handleLifecycleEvent(Lifecycle.Event.ON_START)
-        }
-
-        override fun onActivityResumed(activity: Activity) {
-            if (activity.hashCode() != registrarActivityHashCode) {
-                return
-            }
-            lifecycle.handleLifecycleEvent(Lifecycle.Event.ON_RESUME)
-        }
-
-        override fun onActivityPaused(activity: Activity) {
-            if (activity.hashCode() != registrarActivityHashCode) {
-                return
-            }
-            lifecycle.handleLifecycleEvent(Lifecycle.Event.ON_PAUSE)
-        }
-
-        override fun onActivityStopped(activity: Activity) {
-            if (activity.hashCode() != registrarActivityHashCode) {
-                return
-            }
-            lifecycle.handleLifecycleEvent(Lifecycle.Event.ON_STOP)
-        }
-
-        override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) {}
-
-        override fun onActivityDestroyed(activity: Activity) {
-            if (activity.hashCode() != registrarActivityHashCode) {
-                return
-            }
-            activity.application.unregisterActivityLifecycleCallbacks(this)
-            lifecycle.handleLifecycleEvent(Lifecycle.Event.ON_DESTROY)
-        }
-
-        override fun getLifecycle(): Lifecycle = lifecycle
     }
 }
