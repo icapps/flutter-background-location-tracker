6f180c55d6f3120faeeff023ea82c89ade5534e2
RekanAnwar <rekan545@gmail.com>
2024-11-17 14:23:01 +0300

    Update 1.5.3

--- android/src/main/kotlin/com/icapps/background_location_tracker/flutter/FlutterBackgroundManager.kt
+++ android/src/main/kotlin/com/icapps/background_location_tracker/flutter/FlutterBackgroundManager.kt
@@ -15,14 +15,11 @@ import io.flutter.embedding.engine.plugins.shim.ShimPluginRegistry
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.view.FlutterCallbackInformation
-import kotlin.math.abs
 
 internal object FlutterBackgroundManager {
     private const val BACKGROUND_CHANNEL_NAME = "com.icapps.background_location_tracker/background_channel"
 
     private val flutterLoader = FlutterLoader()
-    private var lastLocation: Location? = null
-    private var lastValidCourse: Float = -1f
 
     private fun getInitializedFlutterEngine(ctx: Context): FlutterEngine {
         Logger.debug("BackgroundManager", "Creating new engine")
@@ -77,25 +74,7 @@ internal object FlutterBackgroundManager {
         data["alt"] = if (location.hasAltitude()) location.altitude else 0.0
         data["vertical_accuracy"] = -1.0
         data["horizontal_accuracy"] = if (location.hasAccuracy()) location.accuracy else -1.0
-        
-        // Enhanced course calculation with smoothing
-        val course = if (location.hasBearing()) {
-            var bearing = location.bearing
-            if (bearing < 0) bearing += 360f
-            
-            // Only update course if speed is above threshold and accuracy is good
-            if (location.hasSpeed() && location.speed > 0.1 && location.hasAccuracy() && location.accuracy < 20) {
-                lastValidCourse = bearing
-                bearing
-            } else if (lastValidCourse >= 0) {
-                // Use last valid course if current reading is not reliable
-                lastValidCourse
-            } else {
-                -1.0f
-            }
-        } else -1.0f
-        
-        data["course"] = course
+        data["course"] = if (location.hasBearing()) location.bearing else -1.0
         data["course_accuracy"] = -1.0
         data["speed"] = if (location.hasSpeed()) location.speed else -1.0
         data["speed_accuracy"] = -1.0
@@ -106,8 +85,6 @@ internal object FlutterBackgroundManager {
             data["course_accuracy"] = if (location.hasBearingAccuracy()) location.bearingAccuracyDegrees else -1.0
             data["speed_accuracy"] = if (location.hasSpeedAccuracy()) location.speedAccuracyMetersPerSecond else -1.0
         }
-
-        lastLocation = location
 
         channel.invokeMethod("onLocationUpdate", data, object : MethodChannel.Result {
             override fun success(result: Any?) {
